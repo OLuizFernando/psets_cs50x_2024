b9ff3ebc42ecd07d390deaec8b76e2330291292d
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-04-26 01:29:04 -0300

    mario-more

---
+++ problem_sets/week_6/sentimental_mario-more/mario.py
@@ -0,0 +1,11 @@
+height = str()
+
+while not height.isnumeric() or int(height) <= 0 or int(height) == 9:
+    height = input("Height: ")
+
+height = int(height)
+
+for i in range(1, height + 1):
+    print(" " * (height - i), end="")
+    print("#" * i, end="  ")
+    print("#" * i)
